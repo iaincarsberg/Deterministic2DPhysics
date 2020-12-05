497ba94138de9ac805bc53b57f7172116f55ccdd
Iain Carsberg <iain.carsberg@mediatonic.co.uk>
2020-12-05 19:14:00 +0000

    removed unused code

--- SveltoDeterministic2DPhysicsDemo/Physics/Descriptors/TransformDescriptor.cs
+++ SveltoDeterministic2DPhysicsDemo/Physics/Descriptors/TransformDescriptor.cs
@@ -6,11 +6,5 @@ namespace SveltoDeterministic2DPhysicsDemo.Physics.Descriptors
 {
     public class TransformDescriptor : GenericEntityDescriptor<TransformEntityComponent, EGIDComponent>
     {
-        public static (NB<TransformEntityComponent>, NB<EGIDComponent>, int) Query(EntitiesDB entitiesDb)
-        {
-            var (transforms, egids, count) = entitiesDb.QueryEntities<TransformEntityComponent, EGIDComponent>(GameGroups.RigidBodyWithBoxCollider);
-        
-            return (transforms, egids, count);
-        }
     }
 }
